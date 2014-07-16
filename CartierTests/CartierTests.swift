f37315b47d364ccac73fe5290c4c8d1cc8e6ee5a
Gianni Settino <gianni@wherecloud.com>
2014-07-17 00:28:52 +0200

    Initial Commit

---
+++ CartierTests/CartierTests.swift
@@ -0,0 +1,35 @@
+//
+//  CartierTests.swift
+//  CartierTests
+//
+//  Created by Gianni Settino on 2014-07-17.
+//  Copyright (c) 2014 Milton and Parc. All rights reserved.
+//
+
+import XCTest
+
+class CartierTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
