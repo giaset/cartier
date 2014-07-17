3f6278a0ebba68040e2e70b7de65de248478809c
Gianni Settino <gianni@wherecloud.com>
2014-07-17 02:36:29 +0200

    Start work on MainViewController

---
+++ Cartier/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  Cartier
+//
+//  Created by Gianni Settino on 2014-07-17.
+//  Copyright (c) 2014 Milton and Parc. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.whiteColor()
+        self.title = "Cartier"
+    }
+    
+}
