d011c0b477e92912cc6c173ff9594c28b6151088
Gianni Settino <g@miltonandparc.com>
2014-07-21 22:23:03 +0200

    Basic demo working with GPUImage

--- Cartier/MainViewController.swift
+++ Cartier/MainViewController.swift
@@ -8,14 +8,33 @@
 
 import UIKit
 import CoreLocation
+import GPUImage
 
 class MainViewController: UIViewController {
+    
+    var videoCamera:GPUImageVideoCamera?
+    var filter:GPUImagePixellateFilter?
+    var videoView:GPUImageView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor.whiteColor()
         self.title = "Cartier"
+        
+        // Initialize the camera
+        videoCamera = GPUImageVideoCamera(sessionPreset: AVCaptureSessionPreset640x480, cameraPosition: .Back)
+        videoCamera!.outputImageOrientation = .Portrait
+        
+        // Initialize the filter
+        filter = GPUImagePixellateFilter()
+        videoCamera?.addTarget(filter)
+        
+        // Create our view, add it as a target of our filter, and star the camera!
+        videoView = GPUImageView(frame: self.view.frame)
+        self.view.addSubview(videoView)
+        filter?.addTarget(videoView)
+        videoCamera?.startCameraCapture()
     }
     
 }
