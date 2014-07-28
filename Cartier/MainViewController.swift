0cf91e63ba79ec9a8f052a2edfa2db81cbd084bc
Gianni Settino <g@miltonandparc.com>
2014-07-28 11:15:49 +0200

    Labbing

--- Cartier/MainViewController.swift
+++ Cartier/MainViewController.swift
@@ -12,10 +12,14 @@ import GPUImage
 
 class MainViewController: UIViewController {
     
+    var backgroundOpacity: Float = 0.6
+    
     var videoCamera: GPUImageVideoCamera?
     var videoView: GPUImageView?
     var iosBlurFilter: GPUImageiOSBlurFilter?
     var coloredOverlayView: UIView?
+    
+    var opacitySlider: UISlider?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,7 +43,7 @@ class MainViewController: UIViewController {
     func setupBackgroundView() {
         coloredOverlayView = UIView(frame: self.view.frame)
         coloredOverlayView!.backgroundColor = UIColor(red: 0.086, green: 0.627, blue: 0.522, alpha: 1)
-        coloredOverlayView!.alpha = 0.6
+        coloredOverlayView!.alpha = backgroundOpacity
         self.view.addSubview(coloredOverlayView)
     }
     
@@ -65,6 +69,8 @@ class MainViewController: UIViewController {
     }
     
     func setupSwitchAndSliders() {
+        println("self.view.frame = \(self.view.frame)")
+        
         var backgroundSwitch = UISwitch(frame: CGRectMake(0.0, 20.0+10.0, 0.0, 0.0)) //size components are ignored
         var modifiedFrame = backgroundSwitch.frame
         modifiedFrame.origin.x = self.view.frame.width - modifiedFrame.width - 5.0
@@ -73,11 +79,31 @@ class MainViewController: UIViewController {
         backgroundSwitch.onTintColor = UIColor.whiteColor()
         backgroundSwitch.on = true
         backgroundSwitch.addTarget(self, action: "switchChanged:", forControlEvents: .ValueChanged)
-        
         self.view.addSubview(backgroundSwitch)
+        println("backgroundSwitch.frame = \(backgroundSwitch.frame)")
+        
+        opacitySlider = UISlider(frame: CGRectMake(5.0, 30.0, 249.0, 31.0))
+        opacitySlider!.tintColor = UIColor.whiteColor()
+        opacitySlider!.thumbTintColor = UIColor.redColor()
+        opacitySlider!.minimumValue = 0.0
+        opacitySlider!.maximumValue = 1.0
+        opacitySlider!.value = backgroundOpacity
+        opacitySlider!.addTarget(self, action: "opacitySliderChanged", forControlEvents: .ValueChanged)
+        self.view.addSubview(opacitySlider)
+        println("opacitySlider.frame = \(opacitySlider!.frame)")
     }
     
     func switchChanged(backgroundSwitch: UISwitch) {
-        UIView.animateWithDuration(1.0, animations: { self.coloredOverlayView!.alpha = backgroundSwitch.on ? 0.6 : 0.0 })
+        UIView.animateWithDuration(1.0, animations: {
+            self.coloredOverlayView!.alpha = backgroundSwitch.on ? self.backgroundOpacity : 0.0
+        })
+        UIView.animateWithDuration(0.5, animations: {
+            self.opacitySlider!.alpha = backgroundSwitch.on ? 1.0 : 0.0
+            })
+    }
+    
+    func opacitySliderChanged() {
+        self.backgroundOpacity = self.opacitySlider!.value
+        self.coloredOverlayView!.alpha = backgroundOpacity
     }
 }
