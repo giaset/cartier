12337901579d211199bef3c920ff3ebe8dc252e4
Gianni Settino <g@miltonandparc.com>
2014-08-03 21:08:45 +0300

    Add AFNetworking to project

---
+++ Cartier/Cartier-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Cartier-Bridging-Header.h
+//  Cartier
+//
+//  Created by Gianni Settino on 2014-08-03.
+//  Copyright (c) 2014 Milton and Parc. All rights reserved.
+//
+
+#import "AFNetworking.h"
