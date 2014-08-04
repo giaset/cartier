08f7f6acdacd44c9591f48120e951f9ea0a5ccd2
Gianni Settino <g@miltonandparc.com>
2014-08-05 01:48:18 +0300

    Progress on getting NetworkingLogic to be a proper Singleton

--- Cartier/MainViewController.swift
+++ Cartier/MainViewController.swift
@@ -143,6 +143,6 @@ class MainViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.stopUpdatingLocation()
         var userLocation: CLLocation = locations[locations.endIndex - 1] as CLLocation
         
-        //NetworkingLogic.findFoursquarePlaces(afNetworkingManager, coordinates: userLocation.coordinate)
+        NetworkingLogic.findFoursquarePlaces(afNetworkingManager, coordinates: userLocation.coordinate)
     }
 }
--- Cartier/NetworkingLogic.swift
+++ Cartier/NetworkingLogic.swift
@@ -9,24 +9,26 @@
 import Foundation
 import CoreLocation
 
-class NetworkingLogic {
+let foursquareClientId = "35UH1ZRV1LML4OMHUNV2CISGII0GHFILV3Z1CHDBQB5WHIO1"
+let foursquareClientSecret = "5NCVL2KJG5WDNS4KX2ZR5SVP3UDMFJNZL04LSHJLLDL5ZY0G"
+let foursquareVersion = "20140803"
+
+class NetworkingLogic: NSObject {
     
     class func findFoursquarePlaces(afNetworkingManager: AFHTTPRequestOperationManager, coordinates: CLLocationCoordinate2D) {
-        let foursquareClientId = "35UH1ZRV1LML4OMHUNV2CISGII0GHFILV3Z1CHDBQB5WHIO1"
-        let foursquareClientSecret = "5NCVL2KJG5WDNS4KX2ZR5SVP3UDMFJNZL04LSHJLLDL5ZY0G"
-        let foursquareVersion = "20140803"
         
-        var urlString = "https://api.foursquare.com/v2/venues/explore?client_id=\(foursquareClientId)&client_secret=\(foursquareClientSecret)&ll=\(coordinates.latitude),\(coordinates.longitude)&v=\(foursquareVersion)"
+        var urlString = "https://api.foursquare.com/v2/venues/explore?client_id=\(foursquareClientId)&client_secret=\(foursquareClientSecret)&ll=\(coordinates.latitude),\(coordinates.longitude)"
         
         func success(operation: AFHTTPRequestOperation!, responseObject: AnyObject!) {
             println(responseObject)
         }
         
-        afNetworkingManager.GET(urlString, parameters: nil, success: success, failure: nil)
+        afNetworkingManager.GET(urlString, parameters: nil, success: success, failure: popError)
     }
     
-    func popError(operation: AFHTTPRequestOperation!, error: NSError!) {
+    class func popError(operation: AFHTTPRequestOperation!, error: NSError!) {
         var errorAlert = UIAlertView(title: "Network Error", message: error.description, delegate: nil, cancelButtonTitle: "Ok")
+        
         errorAlert.show()
     }
     
