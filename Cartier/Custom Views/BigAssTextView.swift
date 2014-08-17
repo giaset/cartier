4fbef851a7118d5bc121f1f2d766c2e622708e0e
Gianni Settino <g@miltonandparc.com>
2014-08-17 18:15:09 +0200

    Start implementing BigAssTextView

---
+++ Cartier/Custom Views/BigAssTextView.swift
@@ -0,0 +1,13 @@
+//
+//  BigAssTextView.swift
+//  Cartier
+//
+//  Created by Gianni Settino on 2014-08-14.
+//  Copyright (c) 2014 Milton and Parc. All rights reserved.
+//
+
+import UIKit
+
+class BigAssTextView: UIView {
+    
+}
